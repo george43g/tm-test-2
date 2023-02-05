69aef14b610f23d6047f69c77a21e2f06dad4b44
George Grigorian <george.g93@gmail.com>
2023-02-05 17:43:32 +1100

    fix(homepage): mobile layout grid css heights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,51 +3,47 @@ import Head from 'next/head';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import MovieCard from '@/components/MovieCard';
-import {
-  SortAccordion,
-  FilterAccordion,
-  WatchAccordion,
-} from '@/components/Nav';
+import { SortAccordion, FilterAccordion, WatchAccordion } from '@/components/Nav';
 import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 type Movie = {
-  id: string | number,
-  name: string,
-  image: string, 
-  date: string,
-  rating: string
-} 
+  id: string | number;
+  name: string;
+  image: string;
+  date: string;
+  rating: string;
+};
 
 export default function Home() {
-  const [ movies, setMovies ] = useState<Movie[]>([])
-  const [ loading, setLoading ] = useState(true)
-  const [ error, setError ] = useState("")
-  const [ endReached, setEndReached ] = useState(false)
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [endReached, setEndReached] = useState(false);
 
-  useEffect(() => fetchMovies(), [])
-  
+  useEffect(() => fetchMovies(), []);
+
   const fetchMovies = () => {
-   setLoading(true)
-   fetch("./api/movies?limit=20") 
-    .then(response => response.json())
-    .then((json) => updateStates(json))
-    .catch((err) => {
-      setLoading(false)
-      setError(err.message)
-    })
-  }
-  
-  const updateStates = (response: { movies: Movie[], count: number}) => {
-    const _movies = [...movies, ...response.movies]
-    setError("")
-    setMovies(_movies)
-    const reached = _movies.length >= response.count
-    console.log({reached})
-    setEndReached(reached)
-    setLoading(false)
-  }
+    setLoading(true);
+    fetch('./api/movies?limit=20')
+      .then(response => response.json())
+      .then(json => updateStates(json))
+      .catch(err => {
+        setLoading(false);
+        setError(err.message);
+      });
+  };
+
+  const updateStates = (response: { movies: Movie[]; count: number }) => {
+    const _movies = [...movies, ...response.movies];
+    setError('');
+    setMovies(_movies);
+    const reached = _movies.length >= response.count;
+    console.log({ reached });
+    setEndReached(reached);
+    setLoading(false);
+  };
 
   return (
     <>
@@ -68,35 +64,27 @@ export default function Home() {
         </nav>
         <div className="overflow-y-auto h-full">
           <div className={styles.movies__grid} data-end={endReached}>
-            {movies.map(({ id, name, image, date, rating}) => (
-              <MovieCard
-                key={id}
-                name={name}
-                image={image}
-                date={date}
-                rating={rating}
-              />
+            {movies.map(({ id, name, image, date, rating }) => (
+              <MovieCard key={id} name={name} image={image} date={date} rating={rating} />
             ))}
           </div>
-            <div className={styles.info_bar}>
-              {error && (
-                <div className={styles.error_box}>{error}</div>
-              )}
-              {loading ? (
-                <button className={styles.loading_more} disabled>
-                  {/* @todo: use the unicode ellipse key here */}
-                  Loading...
-                </button>
-              ) : (
-                <>
-                  {!endReached && (
-                    <button className={styles.load_more} onClick={fetchMovies}>
-                      Load more
-                    </button>
-                  )}
-                </>
-              )}
-            </div>
+          <div className={styles.info_bar}>
+            {error && <div className={styles.error_box}>{error}</div>}
+            {loading ? (
+              <button className={styles.loading_more} disabled>
+                {/* @todo: use the unicode ellipse key here */}
+                Loading...
+              </button>
+            ) : (
+              <>
+                {!endReached && (
+                  <button className={styles.load_more} onClick={fetchMovies}>
+                    Load more
+                  </button>
+                )}
+              </>
+            )}
+          </div>
         </div>
       </main>
     </>
